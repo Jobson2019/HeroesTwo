81806e4bad04d3078fa98b7b7cdf7d9d28972828
Kevin Jobson <jobsonk2877@gmail.com>
2019-10-25 15:18:45 -0500

    first commit for reupload

---
+++ SuperHero/Models/Hero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperHero.Models
+{
+    public class Hero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+
+}
